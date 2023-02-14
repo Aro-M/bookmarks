7973820c91077b36ab73b7337b4542b9fd06864c
Aro-M <ar.militosyan.97@mail.ru>
2023-02-15 00:27:30 +0400

    third_commit_with_a_branch

--- account/forms.py
+++ account/forms.py
@@ -20,6 +20,12 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
@@ -29,3 +35,11 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['date_of_birth','photo']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id) \
+            .filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError(' Email already in use.')
+        return data
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -1,12 +1,12 @@
-
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import  settings
-from django.conf.urls.static import  static
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('account/',include('account.urls')),
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
 ]
 
 if settings.DEBUG:
