49fb8e10181cefe446fef4397bef2efc2027c933
Aro-M <ar.militosyan.97@mail.ru>
2023-02-16 17:00:19 +0400

    add_django_debug_toolbar

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'images.apps.ImagesConfig',
     'actions.apps.ActionsConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+
+INTERNAL_IPS = [
+ '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'bookmarks.urls'
